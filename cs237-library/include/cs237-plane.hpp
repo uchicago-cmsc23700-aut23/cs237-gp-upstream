3b9b2ecf0589f4720276bf85fa7b6603ce3d7a1f
John Reppy <JohnReppy@users.noreply.github.com>
2023-10-27 11:07:37 -0500

    tweaks to support files

--- cs237-library/include/cs237-plane.hpp
+++ cs237-library/include/cs237-plane.hpp
@@ -5,7 +5,7 @@
  * \author John Reppy
  */
 
-/* COPYRIGHT (c) 2022 John Reppy (http://cs.uchicago.edu/~jhr)
+/* COPYRIGHT (c) 2023 John Reppy (http://cs.uchicago.edu/~jhr)
  * All rights reserved.
  */
 
@@ -18,27 +18,27 @@
 
 namespace cs237 {
 
-//! representation of a oriented 3D plane
+/// representation of a oriented 3D plane
 struct Plane {
-    glm::dvec4 _nd;     //! unit normal and signed distance from origin
+    glm::dvec4 _nd;     /// unit normal and signed distance from origin
 
     Plane () { }
 
-    //! \brief specify a plane as a unit-length normal vector and
-    //!        signed distance from origin (single precision)
-    //! \param n unit-length plane normal vector
-    //! \param d signed distance from origin to the plane
+    /// \brief specify a plane as a unit-length normal vector and
+    ///        signed distance from origin (single precision)
+    /// \param n unit-length plane normal vector
+    /// \param d signed distance from origin to the plane
     Plane (glm::vec3 n, float d) : _nd(glm::dvec4(glm::dvec3(n), double(d))) { }
 
-    //! \brief specify a plane as a unit-length normal vector and
-    //!        signed distance from origin (double precision)
-    //! \param n unit-length plane-normal vector
-    //! \param d signed distance from origin to the plane
+    /// \brief specify a plane as a unit-length normal vector and
+    ///        signed distance from origin (double precision)
+    /// \param n unit-length plane-normal vector
+    /// \param d signed distance from origin to the plane
     Plane (glm::dvec3 n, double d) : _nd(glm::dvec4(n, d)) { }
 
-    //! \brief specify a plane as a normal vector and point on the plane (single precision)
-    //! \param n plane-normal vector (does not have to be unit length)
-    //! \param p a point on the plane
+    /// \brief specify a plane as a normal vector and point on the plane (single precision)
+    /// \param n plane-normal vector (does not have to be unit length)
+    /// \param p a point on the plane
     Plane (glm::vec3 n, glm::vec3 p)
     {
         glm::dvec3 norm = glm::normalize(glm::dvec3(n));
@@ -46,9 +46,9 @@ struct Plane {
         this->_nd = glm::dvec4(norm, d);
     }
 
-    //! \brief specify a plane as a normal vector and point on the plane (double precision)
-    //! \param n plane-normal vector (does not have to be unit length)
-    //! \param p a point on the plane
+    /// \brief specify a plane as a normal vector and point on the plane (double precision)
+    /// \param n plane-normal vector (does not have to be unit length)
+    /// \param p a point on the plane
     Plane (glm::dvec3 n, glm::dvec3 p)
     {
         glm::dvec3 norm = glm::normalize(n);
@@ -56,21 +56,21 @@ struct Plane {
         this->_nd = glm::dvec4(norm, d);
     }
 
-    //! \brief get the plane normal vector (double precision)
+    /// \brief get the plane normal vector (double precision)
     glm::dvec3 normd () const { return glm::dvec3(this->_nd); }
 
-    //! \brief get the plane normal vector (single precision)
+    /// \brief get the plane normal vector (single precision)
     glm::vec3 normf () const { return glm::vec3(this->normd()); }
 
-    //! \brief signed distance from origin to plane (double precision)
+    /// \brief signed distance from origin to plane (double precision)
     double distd () const { return this->_nd.w; }
 
-    //! \brief signed distance from origin to plane (single precision)
+    /// \brief signed distance from origin to plane (single precision)
     float distf () const { return float(this->_nd.w); }
 
-    //! \brief signed distance from a point to plane
-    //! \param p a point in space
-    //! \return the signed distance from `p` to the plane
+    /// \brief signed distance from a point to plane
+    /// \param p a point in space
+    /// \return the signed distance from `p` to the plane
     double distanceToPt (glm::dvec3 const &p) const
     {
 	return glm::dot(this->_nd, glm::dvec4(p, 1.0));
--- cs237-library/include/tqt.hpp
+++ cs237-library/include/tqt.hpp
@@ -6,7 +6,7 @@
  */
 
 /*
- * COPYRIGHT (c) 2022 John Reppy (http://cs.uchicago.edu/~jhr)
+ * COPYRIGHT (c) 2023 John Reppy (http://cs.uchicago.edu/~jhr)
  * All rights reserved.
  */
 
@@ -18,50 +18,50 @@
 
 namespace tqt {
 
-  //! Manages a disk-based texture-image quadtree and supports loading individual
-  //! texture images at different levels and locations in the tree.
-    class TextureQTree {
-      public:
+/// Manages a disk-based texture-image quadtree and supports loading individual
+/// texture images at different levels and locations in the tree.
+class TextureQTree {
+  public:
 
-        //! \brief constructor for a texture quad tree
-        //! \param filename
-        //! \param flip should the image be flipped to match OpenGL conventions
-        //! \param sRGB are the textures in sRGB format?
-        TextureQTree (std::string const &filename, bool flip, bool sRGB);
-        ~TextureQTree();
+    /// \brief constructor for a texture quad tree
+    /// \param filename
+    /// \param flip should the image be flipped to match OpenGL conventions
+    /// \param sRGB are the textures in sRGB format?
+    TextureQTree (std::string const &filename, bool flip, bool sRGB);
+    ~TextureQTree();
 
-      //! is this a valid TQT?
-        bool isValid () const { return this->_source != nullptr; }
-      //! the depth of the TQT
-        int depth() const { return this->_depth; }
-      //! the size of a texture tile measured in pixels (tiles are always square)
-        int tileSize() const { return this->_tileSize; }
+    /// is this a valid TQT?
+    bool isValid () const { return this->_source != nullptr; }
+    /// the depth of the TQT
+    int depth() const { return this->_depth; }
+    /// the size of a texture tile measured in pixels (tiles are always square)
+    int tileSize() const { return this->_tileSize; }
 
-      //! \brief return the image tile at the specified quadtree node.
-      //! \param[in] level the level of the node in the tree (root = 0)
-      //! \param[in] row the row of the node on its level (north == 0)
-      //! \param[in] col the column of the node on its level (west == 0)
-      //! \return a pointer to the image; nullptr is returned if there is
-      //!         an error.  It is the caller's responsibility to manage the
-      //!         image's storage.
-        cs237::Image2D *loadImage (int level, int row, int col);
+    /// \brief return the image tile at the specified quadtree node.
+    /// \param[in] level the level of the node in the tree (root = 0)
+    /// \param[in] row the row of the node on its level (north == 0)
+    /// \param[in] col the column of the node on its level (west == 0)
+    /// \return a pointer to the image; nullptr is returned if there is
+    ///         an error.  It is the caller's responsibility to manage the
+    ///         image's storage.
+    cs237::Image2D *loadImage (int level, int row, int col);
 
-      //! are the images sRGB?
-        bool sRGB () const { return this->_sRGB; }
+    /// are the images sRGB?
+    bool sRGB () const { return this->_sRGB; }
 
-      //! return true if the file looks like a TQT file of the right version
-        static bool isTQTFile (std::string const &filename);
+    /// return true if the file looks like a TQT file of the right version
+    static bool isTQTFile (std::string const &filename);
 
-      private:
-        std::vector<std::streamoff> _toc;       //!< stream offsets for images
-        int _depth;                             //!< the depth of the TQT
-        int _tileSize;                          //!< the size of a texture tile in pixels
-        bool _flip;                             //!< true if we are flipping the Y dimension
-                                                //!  of the loaded images
-        bool _sRGB;                             //!< true if we are loading sRGB images
-        std::ifstream *_source;                 //!< the source file for the textures
+  private:
+    std::vector<std::streamoff> _toc;       ///< stream offsets for images
+    int _depth;                             ///< the depth of the TQT
+    int _tileSize;                          ///< the size of a texture tile in pixels
+    bool _flip;                             ///< true if we are flipping the Y dimension
+                                            ///  of the loaded images
+    bool _sRGB;                             ///< true if we are loading sRGB images
+    std::ifstream *_source;                 ///< the source file for the textures
 
-    };  // class TextureQTree
+};  // class TextureQTree
 
 } // namespace tqt
 
